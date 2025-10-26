8fe8c4530ad64b8e2a3a6a8a26eebe523e48bfae
Mohsen <152495454+mohsenforghani@users.noreply.github.com>
2025-10-26 17:07:34 +0330

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+// config.js
+export default {
+  SERVER_PORT: 8080,
+
+  MAX_ROOMS: 10,
+  LOGICAL_W: 450,
+  LOGICAL_H: 800,
+
+  BALL_SPEED: 6,
+  BALL_RADIUS: 8,
+
+  PADDLE_W: 100,
+  PADDLE_H: 14,
+
+  WIN_SCORE: 20,
+
+  TICK_RATE: 1000 / 60,
+  LOBBY_REFRESH: 10000
+};
